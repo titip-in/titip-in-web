1ccf1864ca64c748c0bf6c0d77e5a10676a2c71b
alberdjuniawan <alberdjuniawanpasunda@gmail.com>
2026-04-16 23:10:09 +0700

    feat: add wa_number and avatar_url to user schema

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('wa_number')->unique();
+            $table->string('avatar_url')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });

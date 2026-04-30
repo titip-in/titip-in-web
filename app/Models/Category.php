553a8c453c0a458a00f8f6eaef291dddaf791578
alberdjuniawan <alberdjuniawanpasunda@gmail.com>
2026-04-30 19:37:11 +0700

    feat: add jastip and preloved relations to category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,8 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 #[Fillable(['name', 'icon', 'type'])]
 class Category extends Model
 {
-    public function prelovedItems()
+    public function jastipListings()
     {
-        return $this->hasMany(PrelovedItem::class);
+        return $this->hasMany(JastipListing::class);
+    }
+
+    public function jastipRequest()
+    {
+        return $this->hasMany(JastipRequest::class);
+    }
+
+    public function prelovedListings()
+    {
+        return $this->hasMany(PrelovedListing::class);
+    }
+
+    public function prelovedRequest()
+    {
+        return $this->hasMany(PrelovedRequest::class);
     }
 }

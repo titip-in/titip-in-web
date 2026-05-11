dc30187da9cc779d6e2170e929bd26f7890224ce
alberdjuniawan <alberdjuniawanpasunda@gmail.com>
2026-05-11 12:10:40 +0700

    feat: integrate scramble for automated swagger ui

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Dedoc\Scramble\Scramble;
+use Illuminate\Routing\Route;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('viewApiDocs', function ($user = null) {
+            return true; 
+        });
+
+        Scramble::routes(function (Route $route) {
+            return str($route->uri)->startsWith('api/');
+        });
     }
 }

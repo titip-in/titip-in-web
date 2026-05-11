69de50223ed4081157b5abad6acea092d102ad4d
alberdjuniawan <alberdjuniawanpasunda@gmail.com>
2026-05-11 13:37:07 +0700

    feat: add avatar url attribute to user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -31,6 +31,7 @@ class UserFactory extends Factory
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
             'wa_number' => fake()->unique()->phoneNumber(),
+            'avatar_url' => null,
         ];
     }
 

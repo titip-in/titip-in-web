843020a0a5b76bac1193ea68409a551c442da462
alberdjuniawan <alberdjuniawanpasunda@gmail.com>
2026-05-13 11:14:18 +0700

    fix: force https scheme for assets and fix missing css

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use Dedoc\Scramble\Scramble;
 use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         Gate::define('viewApiDocs', function ($user = null) {
             return true; 
         });

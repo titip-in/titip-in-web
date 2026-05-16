b0992f180185c1622a49d36cc236d791d7f5591c
alberdjuniawan <alberdjuniawanpasunda@gmail.com>
2026-05-16 14:33:41 +0700

    feat: add embedding fillable attributes

--- app/Models/PrelovedRequest.php
+++ app/Models/PrelovedRequest.php
@@ -7,10 +7,14 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-#[Fillable(['user_id', 'category_id', 'title', 'description', 'max_price', 'status'])]
+#[Fillable(['user_id', 'category_id', 'title', 'description', 'max_price', 'status', 'embedding'])]
 class PrelovedRequest extends Model
 {
     use HasUuids, HasFactory;
+
+    protected $hidden = [
+        'embedding',
+    ];
 
     public function user() 
     {

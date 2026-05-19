dfdba0002663e2d2bdb3b23a21a6ab5cbfe63dfa
alberdjuniawan <alberdjuniawanpasunda@gmail.com>
2026-05-19 22:05:03 +0700

    feat: add scheduler for expired tier downgrade

--- routes/console.php
+++ routes/console.php
@@ -9,3 +9,5 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('titipin:close-expired')->everyMinute();
+
+Schedule::command('titipin:downgrade-expired-tiers')->daily();

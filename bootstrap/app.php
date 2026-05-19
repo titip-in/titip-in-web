4c9e7602d5e878ea620e4d931684cbc2da25fd28
alberdjuniawan <alberdjuniawanpasunda@gmail.com>
2026-05-19 18:30:08 +0700

    feat: add admin, banned, and item limit middlewares

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
          
          $middleware->alias([
             'profile.completed' => \App\Http\Middleware\EnsureProfileCompleted::class,
+            'admin' => \App\Http\Middleware\IsAdmin::class,
+            'check.limit' => \App\Http\Middleware\CheckItemLimit::class,
+            'check.banned' => \App\Http\Middleware\CheckBanned::class,
          ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
